24d2d937adcab11f6956a0dad043f880f9e22893
Andres F. Gonzalez <gvnzalez@outlook.com>
2021-06-22 21:41:22 -0600

    Listo

---
+++ FOR/for.js
@@ -0,0 +1,11 @@
+/* Un bucle que se ejecute de 0 a 9 */
+let i;
+for (i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+/* Cree un bucle que recorra cada elemento de la matriz de frutas. */
+const fruits = ["Apple", "Banana", "Orange"]; 
+for (x of fruits) {
+  console.log(x);
+}
---
+++ WHILE/while.js
@@ -0,0 +1,14 @@
+/* Cree un bucle que se ejecute siempre que i sea menor que 10 */
+let i = 0;
+while (i < 10) {
+  console.log(i);
+  i++
+}
+
+/* Cree un ciclo que se ejecute siempre que i sea menor que 10,
+ pero aumente i con 2 cada vez. */
+ let i = 0;
+while (i < 10) {
+  console.log(i);
+  i = i + 2;
+}
